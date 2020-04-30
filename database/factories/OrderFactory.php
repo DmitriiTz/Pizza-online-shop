891acad43528f2ba75a9d1880a827d8f884f36ee
Cibulsky Dmitry <justepicman@gmail.com>
2020-04-30 23:23:02 +0300

    Create factory for Order

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Entities\Core\User;
+use App\Order\Order;
+use Faker\Generator as Faker;
+
+$factory->define(Order ::class, function (Faker $faker) {
+
+    $users = User::all();
+
+    return [
+        'user_id' => $faker->randomElement($users),
+        'cost' => $faker->randomFloat(2, 0, 500),
+    ];
+});
