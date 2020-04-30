71e8603535f944be2e02860db2c77bf31ac2bb54
Cibulsky Dmitry <justepicman@gmail.com>
2020-04-30 23:16:17 +0300

    Create model Pizza

---
+++ app/Entities/Pizza/Pizza.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Pizza;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @SWG\Definition(
+ *  definition="Pizza",
+ *  @SWG\Property(
+ *      property="id",
+ *      type="integer"
+ *  ),
+ *  @SWG\Property(
+ *      property="title",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="description",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="cost",
+ *      type="float"
+ *  )
+ * )
+ */
+class Pizza extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'cost',
+    ];
+
+    /**
+     * The relations to eager load on every query.
+     *
+     * @var array
+     */
+    protected $with = [
+        'covers',
+        'tags'
+    ];
+
+    /**
+     *
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function covers(){
+        return $this->hasMany(PizzasCover::class);
+    }
+
+    /**
+     *
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tags(){
+        return $this->hasMany(PizzasTag::class);
+    }
+}
