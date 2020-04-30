70bf45b174368851240c6d5cae96b1af1b5ca844
Cibulsky Dmitry <justepicman@gmail.com>
2020-04-30 23:15:45 +0300

    Create model Order

---
+++ app/Entities/Order/Order.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Order;
+
+use App\Entities\Core\User;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Order extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'cost',
+    ];
+
+    /**
+     * The relations to eager load on every query.
+     *
+     * @var array
+     */
+    protected $with = [
+        'order_pizzas',
+        'user',
+    ];
+
+    /**
+     *
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function order_pizzas()
+    {
+        return $this->hasMany(OrderPizzas::class);
+    }
+
+    /**
+     *
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
