17882944274609ffa3c5dde6798a12b92a7f2162
Cibulsky Dmitry <justepicman@gmail.com>
2020-04-30 23:18:41 +0300

    Create controller PizzasCover

---
+++ app/Http/Controllers/V1/Pizza/PizzasCoverController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\V1\Pizza;
+
+use App\Http\Controllers\Controller;
+use App\Pizza\PizzasCover;
+use Illuminate\Http\Request;
+
+class PizzasCoverController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Pizza\PizzasCover  $image
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PizzasCover $image)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Pizza\PizzasCover  $image
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PizzasCover $image)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Pizza\PizzasCover  $image
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PizzasCover $image)
+    {
+        //
+    }
+}
